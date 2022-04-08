860e3e2c86b8ab4e84e88a1a41480f20fafbe677
Max <89795178+maxmandia@users.noreply.github.com>
2022-04-08 10:19:59 -0400

    Almost finished the etch-a-sketch project

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-let board = document.querySelector(".board");
-
-board.style.gridTemplateColumns = "repeat(16 , 1fr)";
-board.style.gridTemplateRows = "repeat(16 , 1fr)";
-
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "blue";
-  board.insertAdjacentElement("beforeend", square);
+function changeSize(input) {
+  if (input >= 100) {
+    populateBoard(input);
+  } else {
+    console.log("buggin");
+  }
 }
+
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let btn = document.querySelector(".clear-btn");
+  let squares = board.querySelectorAll(".div");
+  squares.forEach((div) => div.remove());
+
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+
+    square.addEventListener("mouseover", function () {
+      square.style.backgroundColor = "black";
+      btn.addEventListener("click", function () {
+        square.style.backgroundColor = "white";
+      });
+    });
+  }
+}
+
+populateBoard(16);
